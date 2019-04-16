cc0a131b7e24714a1eb495efd71b79c14cf3176e
Kevin Mahoney <kmahoney@theatlantic.com>
2019-04-16 13:45:45 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Mahoney, Kevin on 4/16/19.
+//  Copyright © 2019 Mahoney, Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
