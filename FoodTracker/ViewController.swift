c5fb4be20f3ed99fac07c41d9f25f4b68de57595
Kevin Mahoney <kmahoney@theatlantic.com>
2019-04-18 11:46:53 -0400

    sync text field to label

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
-    
     @IBOutlet weak var nameTextField: UITextField!
 
     @IBOutlet weak var mealNameLabel: UILabel!
@@ -26,8 +25,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //MARK: Actions
     
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "你好"
     }
